6626879a46b7369e63ecf79385882deb7a3dec70
vyk <vyncem@gmail.com>
2025-10-06 13:37:09 +0300

    round plus svg

--- app/ui/archiveTile.js
+++ app/ui/archiveTile.js
@@ -490,7 +490,7 @@ module.exports.empty = function(state, emit) {
         }
       }}"
     >
-      <svg class="w-10 h-10 link-primary">
+      <svg class="w-10 h-10 link-plus-primary">
         <use xlink:href="/${assets.get('addfiles.svg')}#plus" />
       </svg>
       <div class="pt-6 pb-2 text-center text-lg font-bold tracking-wide">
